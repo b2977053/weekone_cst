5788f7e4b5d8a8ad3a6f920fb0b54d13f6d3520f
cst-NB <stno2cnyes@gmail.com>
2018-07-14 02:55:28 +0800

    JQ ajax 查筆數 丟到 「客戶資料」列表

---
+++ weekone_cst/Controllers/AjaxController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using weekone_cst.Models;
+
+namespace weekone_cst.Controllers
+{
+    public class AjaxController : Controller
+    {
+        // GET: Ajax
+        public ActionResult bankCount(int? cid)
+        {
+            客戶銀行資訊Repository repo = RepositoryHelper.Get客戶銀行資訊Repository();
+            var 銀行資訊 = repo.All();
+            銀行資訊 = 銀行資訊.Where(r => r.客戶Id == cid);
+
+            return Json(new
+            {
+                isOK = true, count = 銀行資訊.Count()
+            }, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult ContactCount(int? cid)
+        {
+            客戶聯絡人Repository repo = RepositoryHelper.Get客戶聯絡人Repository();
+            var 聯絡人 = repo.All();
+            聯絡人 = 聯絡人.Where(r => r.客戶Id == cid);
+
+            return Json(new
+            {
+                isOK = true,
+                count = 聯絡人.Count()
+            }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
